65398af318a008775c762b24815f6461df1e37da
mt-xing <mt-xing@users.noreply.github.com>
2024-10-27 13:11:03 -0400

    Basic eslint setup

--- shared/player.ts
+++ shared/player.ts
@@ -1,7 +1,7 @@
 import { Answer } from "./question";
 
 export type Player = {
-    name: string;
-    score: number;
-    answers: (Answer | null)[];
+  name: string;
+  score: number;
+  answers: (Answer | null)[];
 };
