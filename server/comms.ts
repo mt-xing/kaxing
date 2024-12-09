2e479f9ef2cf1f091effb3836892faf61c475043
mt-xing <mt-xing@users.noreply.github.com>
2024-12-08 19:13:37 -0500

    Also send player ranks to devices after game leaderboard

--- server/comms.ts
+++ server/comms.ts
@@ -181,5 +181,17 @@ export default class Communicator {
         points: x.score,
       })),
     });
+    setTimeout(() => {
+      const ranks = computeRanks(this.#players);
+
+      this.#players.forEach((p) => {
+        const rank = ranks.get(p) ?? Infinity;
+        const msg: GameStateClientResponse = {
+          t: "text",
+          text: `Rank ${rank} with ${Math.round(p.score)} points`,
+        };
+        p.socket.emit("gameState", JSON.stringify(msg));
+      });
+    }, 16000);
   }
 }
