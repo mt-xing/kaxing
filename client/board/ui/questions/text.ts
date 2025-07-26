673cc616914b48a89bc54532c3356d29f8fe03aa
mt-xing <mt-xing@users.noreply.github.com>
2025-07-26 18:01:24 -0400

    Text Board Question

--- client/board/ui/questions/text.ts
+++ client/board/ui/questions/text.ts
@@ -1,27 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Dom from "../../../dom.js";
-import { Question } from "../../../../shared/question.js";
 import { stopAudio } from "../../audio.js";
+import { Question, QuestionResults } from "../../../../shared/question.js";
+import QuestionBoard from "./base.js";
 
-export default class TextQuestionBoard {
-  #wrap: HTMLElement;
-
-  constructor(parent: HTMLElement, question: Extract<Question, { t: "text" }>) {
-    this.#wrap = Dom.div(undefined, "questionWrap textWrap");
-
-    this.#wrap.appendChild(Dom.div(Dom.p(question.text), "questionContent"));
-
-    if (question.img !== undefined) {
-      const img = document.createElement("IMG") as HTMLImageElement;
-      img.src = question.img;
-      img.alt = "";
-      this.#wrap.appendChild(Dom.div(img, "imgWrap"));
-      this.#wrap.classList.add("hasImg");
-    }
-
-    Dom.insertEl(this.#wrap, parent).then(() => {
-      this.#wrap.style.transform = "translateY(0)scale(1)";
-    });
+export default class TextQuestionBoard extends QuestionBoard {
+  constructor(
+    parent: HTMLElement,
+    question: Extract<Question, { t: "text" }>,
+    numPlayers: number,
+  ) {
+    super(parent, question, numPlayers, Dom.div());
   }
 
   showAnswers() {}
@@ -32,10 +21,12 @@ export default class TextQuestionBoard {
 
   setNumAnswers(_numAnswers: number, _numPlayers: number) {}
 
-  showResults(_responses: number[], _numPlayers: number) {}
+  showResults(_results: QuestionResults, _numPlayers: number) {}
 
-  async remove() {
-    await Dom.deleteOuterwrap(this.#wrap);
-    stopAudio();
-  }
+  protected showAnswersInner(): void {}
+
+  protected showResultsInner(
+    _results: QuestionResults,
+    _numPlayers: number,
+  ): void {}
 }
