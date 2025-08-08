d1c5b243d9533c51137937153368d08c7dd1a23a
mt-xing <mt-xing@users.noreply.github.com>
2025-08-07 22:34:10 -0400

    Buggy Range on Map Editor

--- client/editor/src/components/questions/map/answers.tsx
+++ client/editor/src/components/questions/map/answers.tsx
@@ -1,23 +1,34 @@
 import type { Question } from "@shared/question";
 import "./map.css";
-import { Circle, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  Circle,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvent,
+} from "react-leaflet";
 import haversineDistance from "haversine-distance";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import L from "leaflet";
 
 export type MapQuestionEditorProps = {
   q: Extract<Question, { t: "map" }>;
   modify: (newQ: Question) => void;
 };
 
-type AddState = "pin" | "range center" | "range radius";
+type AddState =
+  | { t: "pin" }
+  | { t: "range center" }
+  | { t: "range radius"; center: [number, number] };
 
 function getInstruction(state: AddState | null): string {
   if (!state) {
     return "";
   }
-  switch (state) {
+  switch (state.t) {
     case "pin":
-      return "Click on the map to add an answer pin";
+      return "Click on the map to add a sample answer pin";
     case "range center":
       return "Click on the map to choose where to center an acceptance range";
     case "range radius":
@@ -25,9 +36,73 @@ function getInstruction(state: AddState | null): string {
   }
 }
 
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+function MapClickPassthrough(props: {
+  onClick: (lat: number, lng: number) => void;
+}) {
+  useMapEvent("click", (evt) => {
+    const { lat, lng } = evt.latlng;
+    props.onClick(lat, lng);
+  });
+  return null;
+}
+
 export default function MapQuestionAnswers(props: MapQuestionEditorProps) {
   const { q, modify } = props;
   const [addState, setAddState] = useState<AddState | null>(null);
+  const handleClick = useCallback(
+    (lat: number, lng: number) => {
+      if (!addState) {
+        return;
+      }
+      switch (addState.t) {
+        case "pin":
+          modify({
+            ...q,
+            correct: {
+              ...q.correct,
+              representativeAnswers: q.correct.representativeAnswers.concat([
+                [lat, lng],
+              ]),
+            },
+          });
+          setAddState(null);
+          return;
+        case "range center":
+          setAddState({ t: "range radius", center: [lat, lng] });
+          return;
+        case "range radius":
+          modify({
+            ...q,
+            correct: {
+              ...q.correct,
+              matches: q.correct.matches.concat([
+                {
+                  center: addState.center,
+                  radius: Math.sqrt(
+                    Math.pow(addState.center[0] - lat, 2) +
+                      Math.pow(addState.center[1] - lng, 2),
+                  ),
+                },
+              ]),
+            },
+          });
+          setAddState(null);
+          return;
+      }
+    },
+    [addState, modify, q],
+  );
   return (
     <>
       <p>
@@ -35,16 +110,16 @@ export default function MapQuestionAnswers(props: MapQuestionEditorProps) {
           className="bigbtn"
           disabled={addState !== null}
           onClick={() => {
-            setAddState("pin");
+            setAddState({ t: "pin" });
           }}
         >
-          Add Answer Pin
+          Add Sample Answer Pin
         </button>
         <button
           className="bigbtn"
           disabled={addState !== null}
           onClick={() => {
-            setAddState("range center");
+            setAddState({ t: "range center" });
           }}
         >
           Add Accepted Range
@@ -56,6 +131,7 @@ export default function MapQuestionAnswers(props: MapQuestionEditorProps) {
         zoom={q.startZoom}
         style={{ height: "calc(60vh - 300px)" }}
       >
+        <MapClickPassthrough onClick={handleClick} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -74,22 +150,56 @@ export default function MapQuestionAnswers(props: MapQuestionEditorProps) {
             key={JSON.stringify(match.center)}
           >
             <Popup>
-              <button className="bigbtn popupBtn">Delete This Range</button>
+              <button
+                className="bigbtn popupBtn"
+                onClick={() => {
+                  modify({
+                    ...q,
+                    correct: {
+                      ...q.correct,
+                      matches: q.correct.matches.filter(
+                        (candidate) =>
+                          candidate.radius !== match.radius ||
+                          candidate.center !== match.center,
+                      ),
+                    },
+                  });
+                }}
+              >
+                Delete This Range
+              </button>
             </Popup>
           </Circle>
         ))}
         {q.correct.representativeAnswers.map((answer) => (
-          <Marker position={answer}>
+          <Marker position={answer} key={`${answer[0]}-${answer[1]}`}>
             <Popup>
               <button
                 style={{ position: "relative", zIndex: 2 }}
                 className="bigbtn"
+                onClick={() => {
+                  modify({
+                    ...q,
+                    correct: {
+                      ...q.correct,
+                      representativeAnswers:
+                        q.correct.representativeAnswers.filter(
+                          (candidate) =>
+                            candidate[0] !== answer[0] ||
+                            candidate[1] !== answer[1],
+                        ),
+                    },
+                  });
+                }}
               >
                 Delete This Pin
               </button>
             </Popup>
           </Marker>
         ))}
+        {addState && addState.t === "range radius" ? (
+          <Marker position={addState.center} icon={redIcon} />
+        ) : null}
       </MapContainer>
     </>
   );
